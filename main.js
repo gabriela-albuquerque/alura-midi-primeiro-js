f2fb0e999a7e48a8b5022adb6fc3e5d63842079b
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-05-29 10:37:34 -0300

    criação do laço de repetiçao da rotina de execução

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+//Pom
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
